4e3ae5e9e74ec3fbda4b00159284a9f08b75b4ef
alireza rahimi <alireza.ir76@gmail.com>
2023-11-16 19:48:00 +0330

    CSS and bootstrap
    so know i force to CSS code and bootstrap Class in my code just for navbar

--- innolink/src/Component/Header/Navbar/Navbar.js
+++ innolink/src/Component/Header/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import './Navbar.css';
 const Navbar= ()=>{
     return(
         <nav className="navbar">
-            <ul>
+            <ul className="container">
                 <li><Link to="/">صقحه اصلی</Link></li>
                 <li><Link to="/Project">پروژه های ما</Link></li>
                 <li><Link to="/ContactUS">تماس با ما</Link></li>
